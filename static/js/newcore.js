836f01a5e4a47d4adc51db6eb3eab40263f98432
themohooks <81331307+themohooks@users.noreply.github.com>
2024-09-14 22:57:44 +0300

    fixes

--- static/js/newcore.js
+++ static/js/newcore.js
@@ -98,16 +98,3 @@
 
 
 
-function errimg() {
-    const content = `<center>
-                        <div class="p20 s5" style="border:none; margin:0 -20px; display:none;">
-                            <b>Фото потеряно при крахе винчестера</b>
-                            <div class="sm" style="margin-top:5px">
-                                Если у вас есть это фото, пожалуйста, пришлите его на 
-                                <a href="mailto:admin@transphoto.org?subject=Для восстановления фото 651731">admin@transphoto.org</a>
-                            </div>
-                        </div>
-                    </center>`;
-    $('#err').html(content);
-    $('#err .p20').slideDown(500);
-}
